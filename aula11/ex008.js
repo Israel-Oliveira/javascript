59405fdecf0fb66547694a2902a3e9561a5f84da
IsraelOliveira <104089819+Israel-Oliveira@users.noreply.github.com>
2022-04-26 15:46:49 -0300

    Cont
    Continuação dos estudos

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,2 +1,6 @@
-console.log('O console funcionou corretamente')
-
+var vel = 30.5
+console.log(`A velocidade do seu carro é ${vel} quilometros porrrr hora`)
+if(vel > 60){ //condição simples
+    console.log('Voce ultrapassou a velocidade permitida')
+}
+console.log('Digira com o cinto de segunrança')
