2fd87bd21613d090353f00b4a6d3f05c60424b0f
IsraelOliveira <104089819+Israel-Oliveira@users.noreply.github.com>
2022-06-11 08:52:52 -0300

    Atualização Aula
    Atualização

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -2,8 +2,8 @@ function carregar(){
     var msg = document.getElementById('msg')
     var img = document.getElementById('imagem')
     var data = new Date()
-    var hora = data.getHours()
-
+    //var hora = data.getHours()
+    var hora = 22
     msg.innerHTML = `Agora são ${hora} horas`
     if(hora >=0 && hora <12){
         //bom dia 
---
+++ aula12ex/ex015/script.js
@@ -0,0 +1,24 @@
+function verificar(){
+  var data = new Date()
+  var ano = data.getFullYear()
+  var fAno = document.getElementById('txtano')
+  var res = document.getElementById('res')
+
+  if(fAno.value.length == 0 || fAno.value > ano){
+    alert('Erro')
+  }else{
+    var fsex = document.getElementsByName('radsex')
+    var idade = ano - Number(fAno.value)
+    var genero = ''
+    var img = document.createElement('img')
+    
+
+    if (fsex[0].checked) {
+      genero = 'Homem'
+    }else if (fsex[1].checked) {
+      genero = 'Mulher'
+    }
+    res.style.textAlign = 'center'
+    res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+  }
+}
